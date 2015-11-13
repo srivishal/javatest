08a3091a2840af580fa66633af9d5b38c54c136b
“vishal <Vishal.srivastava@NDI-LAP-309.3pillar.corp>
2015-11-13 05:48:52 -0500

    testing

--- src/jenkins/test.java
+++ src/jenkins/test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class test {
@@ -12,9 +13,17 @@ public class test {
 	
 	@Test 
 	public void StartPage(){
+		  
+		Assert.fail();
+		//System.out.println("openpage");
+		//driver.navigate().to("https://google.com");
+		//System.out.println("pageopened");
+	}
+	@Test 
+	public void TestCase1(){
 		   
 		System.out.println("openpage");
-		driver.navigate().to("https://google.com");
+		//driver.navigate().to("https://google.com");
 		System.out.println("pageopened");
 	}
 }
